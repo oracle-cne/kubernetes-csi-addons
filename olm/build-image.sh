3826f7d91467834e7828fcacc89a4c59c157dba3
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-04-02 23:13:44 +0000

    enable yum repos

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -9,6 +9,9 @@ version="0.12.0"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
 
+#cp /etc/yum.repos.d/ol_artifacts.repo ./
+cp /etc/yum.repos.d/* ./
+
 "${CONTAINER_CLI}" build --pull \
     --build-arg https_proxy=${https_proxy} \
     -t ${docker_tag} -f ./olm/builds/Dockerfile .
