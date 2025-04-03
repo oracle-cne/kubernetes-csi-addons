61d71d9b3854ea4712d390768e046a6dc056beb0
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-04-03 10:01:06 -0700

    mount host yum repos onto the container

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -9,9 +9,7 @@ version="0.12.0"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
 
-#cp /etc/yum.repos.d/ol_artifacts.repo ./
-cp /etc/yum.repos.d/* ./
-
 "${CONTAINER_CLI}" build --pull \
     --build-arg https_proxy=${https_proxy} \
-    -t ${docker_tag} -f ./olm/builds/Dockerfile .
+    --volume /etc/yum.repos.d:/etc/yum.repos.d \
+    --tag ${docker_tag} -f ./olm/builds/Dockerfile .
